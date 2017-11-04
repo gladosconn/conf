b8e1d53a1ff8bda4c47ee3ff10b8c35d506e55f5
GLaDOS <gladosconn@gmail.com>
2017-11-04 15:47:51 +0800

    Add install script for debian.

---
+++ home/inst-debian.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo Downloading VundleVim ...
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo Copying .vimrc file ...
+cp .vimrc ~/.vimrc
+cp .ycm_extra_conf.py ~/.ycm_extra_conf.py
+cp .gitconfig ~/.gitconfig
+
+echo Install plugins ...
+vim +PluginInstall
